9adc26f047af6ea6f4fa454aef3ecbe0cb2750e5
Amir Kenesbay <amirkenesbay7@gmail.com>
2022-06-12 15:58:33 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// тип_переменной название_переменной = значение_переменной
+// prompt(текст, стандартное значение);
+
+let width = null;
+let height = null;
+let area = (height ?? 100) * (width ?? 50);
+alert(area);
